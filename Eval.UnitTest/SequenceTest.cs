6bca2f642964d3cbd31eba853f7a2ccb4426abb0
Michael L Perry <michael@qedcode.com>
2013-05-31 10:45:02 -0500

    Initial commit.

---
+++ Eval.UnitTest/SequenceTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Eval.UnitTest
+{
+    [TestClass]
+    public class SequenceTest
+    {
+        [TestMethod]
+        public void CanGetOddNumbers()
+        {
+            List<int> odds = Sequence
+                .GetOddNumbers(new int[] { 1, 2, 3, 6, 7, 9, 12, 16, 17 })
+                .ToList();
+
+            Assert.IsTrue(odds.Contains(1));
+            Assert.IsTrue(odds.Contains(3));
+            Assert.IsTrue(odds.Contains(7));
+            Assert.IsTrue(odds.Contains(9));
+            Assert.IsTrue(odds.Contains(17));
+        }
+
+        [TestMethod]
+        public void CanExamineInfiniteSet()
+        {
+            List<int> odds = Sequence
+                .GetOddNumbers(Sequence.GetPositiveIntegers())
+                .Take(5)
+                .ToList();
+
+            Assert.IsTrue(odds.Contains(1));
+            Assert.IsTrue(odds.Contains(3));
+            Assert.IsTrue(odds.Contains(5));
+            Assert.IsTrue(odds.Contains(7));
+            Assert.IsTrue(odds.Contains(9));
+        }
+    }
+}
---
+++ Eval/Sequence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eval
+{
+    public class Sequence
+    {
+        public static IEnumerable<int> GetOddNumbers(IEnumerable<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<int> GetPositiveIntegers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
